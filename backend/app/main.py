7aa86c807f9f880bb5ac081818e202479b6f89df
sangoaz <kevin.fruchon@gmail.com>
2026-05-27 21:02:39 +0200

    Déploiement Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://briansbuzzer.vercel.app/",
+        "https://briansbuzzer.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
