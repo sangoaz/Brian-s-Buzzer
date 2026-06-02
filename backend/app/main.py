9946e22af826f26fb467c5971dd27e09e9e60772
sangoaz <kevin.fruchon@gmail.com>
2026-06-02 17:45:21 +0200

    Mise à jour du CORS pour vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://briansbuzzer.vercel.app",
+        "brian-s-buzzer.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
